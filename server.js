9c57ebac80149abdd4eb45fd020ce4ab68c03522
ranjeet kumar <ranjeetgupta156@gamil.com>
2018-04-09 01:41:21 +0530

    =initial commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path');
+
+var app = express();
+app.use(express.static(path.resolve(__dirname)));
+
+app.get('/',(req,res)=>{
+  res.sendFile(__dirname + '/index.html');
+});
+app.get('/animation',(req,res)=>{
+  res.sendFile(__dirname + '/anim.html');
+});
+
+
+
+
+var port = process.env.PORT || 3000;
+app.listen(port,()=>{
+  console.log(`Listening on port ${port}.`);
+})
